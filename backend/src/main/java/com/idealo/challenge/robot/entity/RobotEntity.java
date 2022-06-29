4b93f635d04146d6e9bc24f050876d159972bd05
Ajay Laddha <ajay.laddha4@gmail.com>
2022-06-29 12:18:37 +0530

    Update RobotEntity.java

--- backend/src/main/java/com/idealo/challenge/robot/entity/RobotEntity.java
+++ backend/src/main/java/com/idealo/challenge/robot/entity/RobotEntity.java
@@ -27,95 +27,96 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RobotEntity {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "code")
 	private String code;
-	
+
 	@JoinColumn(name = "position_id")
 	@OneToOne(cascade = CascadeType.ALL)
-    private PositionEntity position;
-    
-    public RobotPosition toDto() {
-    	return RobotPosition.builder()
-    			.id(code)
-    			.x(this.position.getX())
-    			.y(this.position.getY())
-    			.direction(this.position.getDirection()).build();
+	private PositionEntity position;
+
+	public RobotPosition toDto() {
+		return RobotPosition.builder().id(code).x(this.position.getX()).y(this.position.getY())
+				.direction(this.position.getDirection()).build();
 	}
-	
-    /**
-     * Based on the command it will change the position of Robot
-     * @param commandString
-     */
+
+	/**
+	 * Based on the command it will change the position of Robot
+	 * 
+	 * @param commandString
+	 */
 	public void moveRobot(final String commandString) {
-    	String commands[] = commandString.split(" ");
-    	if(commands.length==0) {
-    		throw new InvalidCommandException("No Command");
-    	}
-    	
-		switch (commands[0]) {
-			case "POSITION":
-				if(commands.length<4) {
-		    		throw new InvalidCommandException("Position arguments are not valid. It should have X, Y, Direction",commandString);
-		    	}
-				
-				int x = NumberUtils.getNumber(commands[1]);
-				int y = NumberUtils.getNumber(commands[2]);
-				Direction direction = Direction.getDirection(commands[3]);
-				
-				if(x<0 || y<0 || direction == null) {
-		    		throw new InvalidCommandException("Position arguments are not valid. X, Y, Direction are not valid",commandString);
-		    	}
-				
-				this.position = PositionEntity.builder().x(x).y(y).direction(direction).build();
-				break;
-			case "WAIT":
-				break;
-			case "FORWARD":
-				if(this.position == null) {
-					throw new InvalidCommandException("Initial Position should be first command",commandString);
-		    	}
-			
-				if(commands.length<2) {
-		    		throw new InvalidCommandException("No Command");
-		    	}
-				int move = NumberUtils.getNumber(commands[1]);
-				if(move<0 ) {
-		    		throw new InvalidCommandException("Forward Sub Commands is not valid. Steps are are not valid",commandString);
-		    	}
-				this.position.move(move);
-				break;
-	
-			case "TURNAROUND":
-				if(this.position == null) {
-					throw new InvalidCommandException("Initial Position should be first command",commandString);
-		    	}
-				
-				this.position.turnaround();
-				break;
-	
-			case "RIGHT":
-				if(this.position == null) {
-					throw new InvalidCommandException("Initial Position should be first command",commandString);
-		    	}
-				
-				this.position.right();
-				break;
-			
-			case "LEFT":
-				if(this.position == null) {
-					throw new InvalidCommandException("Initial Position should be first command",commandString);
-		    	}
-				
-				this.position.left();
-				break;
-	
-			default:
-				break;
+		String commands[] = commandString.split(" ");
+		if (commands.length == 0) {
+			throw new InvalidCommandException("No Command");
 		}
-    }
+
+		switch (commands[0]) {
+		case "POSITION":
+			if (commands.length < 4) {
+				throw new InvalidCommandException("Position arguments are not valid. It should have X, Y, Direction",
+						commandString);
+			}
+
+			int x = NumberUtils.getNumber(commands[1]);
+			int y = NumberUtils.getNumber(commands[2]);
+			Direction direction = Direction.getDirection(commands[3]);
+
+			if (x < 0 || y < 0 || direction == null) {
+				throw new InvalidCommandException("Position arguments are not valid. X, Y, Direction are not valid",
+						commandString);
+			}
+
+			this.position = PositionEntity.builder().x(x).y(y).direction(direction).build();
+			break;
+		case "WAIT":
+			break;
+		case "FORWARD":
+			if (this.position == null) {
+				throw new InvalidCommandException("Initial Position should be first command", commandString);
+			}
+
+			if (commands.length < 2) {
+				throw new InvalidCommandException("No Command");
+			}
+			int move = NumberUtils.getNumber(commands[1]);
+			if (move < 0) {
+				throw new InvalidCommandException("Forward Sub Commands is not valid. Steps are are not valid",
+						commandString);
+			}
+			this.position.move(move);
+			break;
+
+		case "TURNAROUND":
+			if (this.position == null) {
+				throw new InvalidCommandException("Initial Position should be first command", commandString);
+			}
+
+			this.position.turnaround();
+			break;
+
+		case "RIGHT":
+			if (this.position == null) {
+				throw new InvalidCommandException("Initial Position should be first command", commandString);
+			}
+
+			this.position.right();
+			break;
+
+		case "LEFT":
+			if (this.position == null) {
+				throw new InvalidCommandException("Initial Position should be first command", commandString);
+			}
+
+			this.position.left();
+			break;
+
+		default:
+			break;
+		}
+	}
 }
